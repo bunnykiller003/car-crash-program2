a2e996433beb150c4e3fa845d6a37abeb1365a7a
bunnykiller003 <63455593+bunnykiller003@users.noreply.github.com>
2020-05-12 20:01:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,16 +6,17 @@ b1=createSprite(1100,100,30,30);
 b2=createSprite(1100,200,30,30);
 b3=createSprite(1100,300,30,30);
 wall=createSprite(50,200,10,600)
+  b1.velocityX=-55;
+  b2.velocityX=-90;
+  b3.velocityX=-60;
+   b1.shapeColor="red";
+  b2.shapeColor="yellow";
+  b3.shapeColor="green";
 }
 
 function draw() {
   background(255,255,255); 
-  b1.shapeColor="red";
-  b2.shapeColor="yellow";
-  b3.shapeColor="green";
-  b1.velocityX=-55;
-  b2.velocityX=-90;
-  b3.velocityX=-60;
+ 
   
   if (b1.x-wall.x<(b1.width/2+wall.width/2)){
  zenia.velocityX=0
